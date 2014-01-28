576687b3d52f5e581c4919124dd50dde5a6d4d2e
Varun Bhatia <vxb9785@rit.edu>
2014-01-27 21:26:44 -0500

    Initial commit

---
+++ ingredients.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+public class Ingredients {
+	public List<String> ingredientList = new ArrayList<String>();
+	public List<String> ingredientsChosen = new ArrayList<String>();
+	
+	
+	}
+
